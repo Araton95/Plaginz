d210494a548d89cf3c675588306193a0f514992d
Ararat Tonoyan <ararat@doctornet.pro>
2016-10-05 13:52:01 +0300

    Scroll to top function jquery

--- site/assets/js/app.js
+++ site/assets/js/app.js
@@ -29,6 +29,9 @@ $(document).ready(function () {
                 }
             });
     })();
+    $('.header').on('click touch', function () {
+        $("html, body").animate({ scrollTop: 0 }, 500);
+    })
 });
 
 
